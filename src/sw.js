c0e927eecae8dc7c050910831a742a9afbba0f68
System Administrator <root@ABCs-MacBook-Air.local>
2025-08-19 14:54:32 -0700

    l

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,7 @@ self.addEventListener("install", (event) => {
     console.log("Service Worker installing…");
     event.waitUntil(
         caches.open("v1").then((cache) => {
-            return cache.addAll(["/", "/international.html", "/manifest.json"]);
+            return cache.addAll(["/", "/userNotifyRegistration.html", "/manifest.json"]);
         })
     );
 });
